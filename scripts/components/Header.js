19a1d5536dd74dba8535a055aef9c99428de973a
Jefry Guevara <jefryeguevarav@gmail.com>
2022-07-26 23:02:47 -0500

    header added

---
+++ scripts/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return `
+    <header>
+      <div>
+        <h1>Hello world from header</h1>
+      </div>
+    </header>
+  `
+}
+
+export default Header
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 import '../styles/style.css'
+import Header from './components/Header'
 
 document.querySelector('#app').innerHTML = `
+  ${Header()}
   <div>
     <h1> Hola Mundo </h1>
   </div>
