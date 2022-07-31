9031b53819b0775181b98597a5b1a3a1bfc768da
raquinterob <raulandresquinterob@outlook.com>
2022-07-31 10:15:38 -0500

    Add Body.js structure

---
+++ scripts/components/Body.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return `
+    <main>
+      <div id="title"> Charadas </div>
+      <div id="random"> Aleatorio </div>
+      <div>
+      <p>Juega charadas, crea sorteos, amaña premios o penitencias :D</p>
+      </div>
+    </main>
+  `
+}
+
+export default Header
